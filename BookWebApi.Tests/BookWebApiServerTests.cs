311c36bd7e6e966f1224f110f08826fd7606503f
LonghronShen <LonghronShen@hotmail.com>
2018-05-09 11:43:02 +0800

    add tests

--- BookWebApi.Tests/BookWebApiServerTests.cs
+++ BookWebApi.Tests/BookWebApiServerTests.cs
@@ -75,13 +75,56 @@ namespace BookWebApi.Tests
 			// Act
 			this._client.DefaultRequestHeaders.Authorization = this._authenticationHeaderValue;
 
-			var response = await this._client.PostAsync("/api/book", 
+			var response = await this._client.PostAsync("/api/book",
 				new StringContent(JsonConvert.SerializeObject(this.Books[0]), Encoding.UTF8, "application/json"));
 
 			// Assert
 			response = response.EnsureSuccessStatusCode();
 			var responseContent = await response.Content.ReadAsStringAsync();
 			Assert.Equal("1", responseContent);
+		}
+
+		[Fact]
+		public async Task ReturnOkWhenBookDeleted()
+		{
+			// Act
+			this._client.DefaultRequestHeaders.Authorization = this._authenticationHeaderValue;
+
+			var response = await this._client.PostAsync("/api/book",
+				new StringContent(JsonConvert.SerializeObject(this.Books[0]), Encoding.UTF8, "application/json"));
+
+			// Assert
+			response = response.EnsureSuccessStatusCode();
+			var responseContent = await response.Content.ReadAsStringAsync();
+			Assert.Equal("1", responseContent);
+
+			response = await this._client.DeleteAsync("/api/book/1");
+			Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+		}
+
+		[Fact]
+		public async Task ReturnOkWhenPutBook()
+		{
+			// Act
+			this._client.DefaultRequestHeaders.Authorization = this._authenticationHeaderValue;
+
+			var response = await this._client.PostAsync("/api/book",
+				new StringContent(JsonConvert.SerializeObject(this.Books[0]), Encoding.UTF8, "application/json"));
+
+			// Assert
+			response = response.EnsureSuccessStatusCode();
+			var responseContent = await response.Content.ReadAsStringAsync();
+			Assert.Equal("1", responseContent);
+
+			response = await this._client.PutAsync("/api/book/1",
+				new StringContent(JsonConvert.SerializeObject(this.Books[1]), Encoding.UTF8, "application/json"));
+			Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+
+			response = await this._client.GetAsync("/api/book/1");
+			response = response.EnsureSuccessStatusCode();
+			responseContent = await response.Content.ReadAsStringAsync();
+			var item = JsonConvert.DeserializeObject<Book>(responseContent);
+			Assert.Equal(this.Books[1].Title, item.Title);
 		}
 
 	}
