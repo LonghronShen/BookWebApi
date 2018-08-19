b487a076ac9f6890b698aef6983c919b6bfde3e0
LonghronShen <LonghronShen@hotmail.com>
2018-08-19 22:20:55 +0800

    fix

---
+++ BookWebApi.Tests/BookServiceTest.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using Xunit;
+
+namespace BookWebApi.Tests
+{
+
+    public class BookServiceTest
+    {
+
+        private readonly IServiceProvider _serviceProvider;
+
+        public BookServiceTest()
+        {
+            this._serviceProvider = CreateServiceProvider(services =>
+            {
+                services.AddAntiforgery();
+            });
+        }
+
+        public static IServiceProvider CreateServiceProvider(Action<IServiceCollection> configureService)
+        {
+            var coll = new ServiceCollection();
+            configureService(coll);
+            return coll.BuildServiceProvider();
+        }
+
+        [Fact]
+        public void AddTest()
+        {
+            //using (var scope = this._serviceProvider.CreateScope())
+            //{
+            //    var service = scope.ServiceProvider.GetService<IBookService>();
+            //    service.AddAsync(new Book() { });
+            //}
+        }
+
+    }
+
+}
